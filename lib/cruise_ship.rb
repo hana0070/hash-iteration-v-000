4b0b84dd4c4ed92df64f9c66a51332c3578e1f81
HANA LEANDER <hana0070@gmail.com>
2019-04-19 18:49:19 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,5 +10,7 @@ def select_winner(passengers)
   # add the code snippet here!
   winner = ""
   passengers.each do |suite, name|
-    
+    if suite == :suite_a && name.start_with?("A")
+      winter = name
+    end
 end
